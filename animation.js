ae4e331463f07df761642b4c8faaa996461a5d26
kaushal saraswat <kaushal.saraswat@indianic.com>
2024-06-02 23:21:54 +0530

    first commit

---
+++ animation.js
@@ -0,0 +1,197 @@
+
+              window.sr = ScrollReveal();
+              sr.reveal('.weare', {
+                duration: 2000,
+                origin:'left',
+                delay:100,
+                distance:'700px'
+              });
+              window.sr = ScrollReveal();
+              sr.reveal('.caption h1', {
+                duration: 2000,
+                origin:'bottom',
+                delay:100,
+                distance:'700px'
+              });
+               
+            
+              window.sr = ScrollReveal();
+              sr.reveal('.caption h3', {
+                duration: 2000,
+                origin:'bottom',
+                delay:1000,
+                distance:'700px'
+              });
+               
+             
+              window.sr = ScrollReveal();
+              sr.reveal('.presentation', {
+                duration: 2000,
+                // delay:300,
+                origin:'bottom',
+                distance:'1200px'
+              });
+              window.sr = ScrollReveal();
+              sr.reveal('.presentation p', {
+                duration: 1000,
+                // delay:300,
+                origin:'bottom',
+                distance:'2200px'
+              });
+              window.sr = ScrollReveal();
+              sr.reveal('.cta-select', {
+                duration: 1500,
+                // delay:300,
+                origin:'bottom',
+                distance:'2200px'
+              });
+              window.sr = ScrollReveal();
+              sr.reveal('.cta-add', {
+                duration: 1500,
+                // delay:300,
+                origin:'bottom',
+                distance:'2200px'
+              });
+              window.sr = ScrollReveal();
+              sr.reveal('.cover1', {
+                duration: 1000,
+                // delay:300,
+                origin:'bottom',
+                distance:'2200px'
+              });
+              window.sr = ScrollReveal();
+              sr.reveal('.pray', {
+                duration: 1500,
+
+                origin:'left',
+                distance:'2200px'
+              });
+              window.sr = ScrollReveal();
+              sr.reveal('.panel', {
+                duration: 2000,
+                origin:'bottom',
+                distance:'2200px'
+              });
+              window.sr = ScrollReveal();
+              sr.reveal('.cover', {
+                duration: 2000,
+                origin:'bottom',
+                distance:'2200px'
+              });
+              window.sr = ScrollReveal();
+              sr.reveal('.cover h1', {
+                duration: 2000,
+                delay:100,
+                origin:'bottom',
+                distance:'1200px'
+              });
+              
+              window.sr = ScrollReveal();
+              sr.reveal('.cover p', {
+                duration: 1000,
+                delay:500,
+                origin:'bottom',
+                distance:'1200px'
+              });
+              window.sr = ScrollReveal();
+              sr.reveal('#c1', {
+                duration: 1500,
+                delay:200,                
+                origin:'bottom',
+                distance:'800px'
+              });
+              sr.reveal('#c4', {
+                duration: 1500,
+                delay:1000,
+                origin:'bottom',
+                distance:'800px'
+              });
+              sr.reveal('#c2', {
+                duration: 1500,
+                delay:500,
+                origin:'bottom',
+                distance:'800px'
+              });
+              sr.reveal('#c3', {
+                duration: 1500,
+                delay:800,
+                origin:'bottom',
+                distance:'800px'
+              });
+              sr.reveal('#jewlin', {
+                duration: 2000,
+                // delay:500,
+                origin:'left',
+                distance:'800px'
+              });
+              sr.reveal('#carouselExampleControls', {
+                duration: 2000,
+                origin:'bottom',
+                distance:'800px'
+              });
+              sr.reveal('#resources h3', {
+                duration: 1000,
+                origin:'top',
+                distance:'800px'
+              });
+              sr.reveal('#resources h1', {
+                duration: 2000,
+                origin:'bottom',
+                distance:'800px'
+              });
+              sr.reveal('#cp', {
+                duration: 3000,
+                origin:'left',
+                distance:'1800px'
+              });
+              sr.reveal('.section-dark h3', {
+                duration: 2000,
+                origin:'bottom',
+                distance:'800px'
+              });
+              sr.reveal('.sec1 h1', {
+                duration: 2000,
+                origin:'left',
+                distance:'800px'
+              });
+              sr.reveal('.sec1 p', {
+                duration: 2000,
+                origin:'bottom',
+                distance:'800px'
+              });
+              sr.reveal('#img1', {
+                duration: 2000,
+                origin:'bottom',
+                distance:'800px'
+              });
+              sr.reveal('.bq1', {
+                duration: 2000,
+                origin:'bottom',
+                distance:'800px'
+              });
+              sr.reveal('#img2', {
+                duration: 2000,
+                origin:'bottom',
+                distance:'800px'
+              });
+              sr.reveal('.bq2', {
+                duration: 2000,
+                origin:'bottom',
+                distance:'800px'
+              });
+              sr.reveal('.narrow h1', {
+                duration: 2000,
+                origin:'bottom',
+                distance:'800px'
+              });
+              sr.reveal('.narrow p', {
+                duration: 2000,
+                origin:'bottom',
+                distance:'800px'
+              });
+              
+              
+              
+              
+
+                           
---
+++ main.js
@@ -0,0 +1,76 @@
+// Select DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set Initial State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
+
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    // Set Menu State
+    showMenu = false;
+  }
+}
+
+const gap = 16;
+
+const carousel = document.getElementById("carousel"),
+  content = document.getElementById("content"),
+  next = document.getElementById("next"),
+  prev = document.getElementById("prev");
+
+next.addEventListener("click", (e) => {
+  carousel.scrollBy(width + gap, 0);
+  if (carousel.scrollWidth !== 0) {
+    prev.style.display = "flex";
+  }
+  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "none";
+  }
+});
+prev.addEventListener("click", (e) => {
+  carousel.scrollBy(-(width + gap), 0);
+  if (carousel.scrollLeft - width - gap <= 0) {
+    prev.style.display = "none";
+  }
+  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "flex";
+  }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", (e) => (width = carousel.offsetWidth));
+
+const scrollBtn = document.querySelector(".btn-top-scroll");
+const sectionServicer = document.querySelector(".section-services");
+
+scrollBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const s1Coords = sectionServicer.getBoundingClientRect();
+
+  window.scrollTo({
+    left: s1Coords.left + window.window.pageXOffset,
+    top: s1Coords.top + window.window.pageYOffset + 180,
+    behavior: "smooth",
+  });
+});
